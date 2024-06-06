ad32a5e5695a3bd7c160122a38e51f580a9cc396
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-06-06 21:43:03 +0300

    Final version with minor fixes

--- Assignment1/code.c
+++ Assignment1/code.c
@@ -5,10 +5,10 @@
 #define CLEAR_COMMAND "cls"
 
 int text_input();
-int save_text(const char *filename);
-int load_text(const char *filename);
-char getCharAt(char **lines, int lineNumber, int index);
-void insertTextAt(char **lines, int lineNumber, int index, const char *text);
+int save_text(const char* filename);
+int load_text(const char* filename);
+char getCharAt(char** lines, int lineNumber, int index);
+void insertTextAt(char** lines, int lineNumber, int index, const char* text);
 int searchText(char** lines, int lineCount, const char* searchText, int positions[][2]);
 void clearConsole();
 
@@ -16,58 +16,60 @@ char** text_lines = NULL;
 int line_count = 0;
 
 int main() {
-    char command;
+    char command[10];
 
     while (1) {
         clearConsole();
-        printf("Choose the command (or type 'exit' to quit): ");
+        printf("Choose the command: ");
         scanf_s("%s", command, (unsigned)_countof(command));
         getchar();
 
-        if (command, "1") {
+        if (strcmp(command, "1") == 0) {
             text_input();
         }
-        else if (command, "2") {
+        else if (strcmp(command, "2") == 0) {
             printf("Enter the file name for saving: ");
-            char filename;
+            char filename[100];
             scanf_s("%s", filename, (unsigned)_countof(filename));
             save_text(filename);
-            printf("Text has been saved successfully");
+            printf("Text has been saved successfully\n");
         }
-        else if (command, "3") {
+        else if (strcmp(command, "3") == 0) {
             printf("Enter the file name for loading: ");
-            char filename;
+            char filename[100];
             scanf_s("%s", filename, (unsigned)_countof(filename));
             load_text(filename);
-            printf("Text has been loaded successfully");
+            printf("Text has been loaded successfully\n");
         }
-        else if (command, "4") {
+        else if (strcmp(command, "4") == 0) {
             printf("You wrote:\n");
             for (int i = 0; i < line_count; i++) {
                 printf("%s\n", text_lines[i]);
             }
         }
-        else if (command, "5") {
+        else if (strcmp(command, "5") == 0) {
             int lineNumber, index;
-            printf("Enter line number and index: ");
-            scanf_s("%d %d, &lineNumber, &index");
+            printf("Enter the line number along with index: ");
+            scanf_s("%d %d", &lineNumber, &index);
             getchar();
 
             printf("Enter text to insert: ");
-            char text;
+            char text[100];
             fgets(text, sizeof(text), stdin);
+            text[strcspn(text, "\n")] = 0;
 
             insertTextAt(text_lines, lineNumber - 1, index, text);
         }
-        else if (command, "6") {
+        else if (strcmp(command, "6") == 0) {
             printf("Enter text to search: ");
-            char search_text;
+            char search_text[100];
             fgets(search_text, sizeof(search_text), stdin);
+            search_text[strcspn(search_text, "\n")] = 0;
 
-            int positions;
+            int positions[100][2];
             int count = searchText(text_lines, line_count, search_text, positions);
-            if (count=0) {
-                printf("Text not found");
+            if (count == 0) {
+                printf("Text not found\n");
             }
             else {
                 for (int i = 0; i < count; i++) {
@@ -75,20 +77,25 @@ int main() {
                 }
             }
         }
-        else if (command, "help") {
-            printf("1 - text typewriter, 2 - new line, 3 - save the file, 4 - load the file, 5 - show what you wrote, 6 - insert text at position, 7 - search\n");
+        else if (strcmp(command, "help") == 0) {
+            printf("1 - text typewriter, 2 - save the file, 3 - load the file, 4 - show what you wrote, 5 - insert text at position, 6 - search\n");
         }
-        else if (command, "exit") {
+        else if (strcmp(command, "exit") == 0) {
             printf("Exiting the program...\n");
             break;
         }
         else {
-            printf("Invalid command\n");
+            printf("Command was not found\n");
         }
 
         printf("Press Enter to continue...");
         getchar();
     }
+
+    for (int i = 0; i < line_count; i++) {
+        free(text_lines[i]);
+    }
+    free(text_lines);
 
     return 0;
 }
@@ -98,7 +105,7 @@ void clearConsole() {
 }
 
 int text_input() {
-    char text;
+    char text[100];
     printf("Enter text: ");
     fgets(text, sizeof(text), stdin);
     text[strcspn(text, "\n")] = 0;
@@ -110,7 +117,7 @@ int text_input() {
     return 0;
 }
 
-int save_text(const char *filename) {
+int save_text(const char* filename) {
     FILE* file = fopen(filename, "w");
     if (file) {
         for (int i = 0; i < line_count; i++) {
@@ -127,7 +134,7 @@ int save_text(const char *filename) {
 int load_text(const char* filename) {
     FILE* file = fopen(filename, "r");
     if (file) {
-        char line;
+        char line[100];
         for (int i = 0; i < line_count; i++) {
             free(text_lines[i]);
         }
